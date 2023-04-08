530a46045d00dabe2ba2da04733d0d1c48676cdb
BORA <a52447879@gmail.com>
2023-04-08 13:15:42 +0900

    feat: add test-helpers

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,7 +3,10 @@ import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 
-const StyledLayoutContainer = styled.div``;
+const StyledLayoutContainer = styled.div`
+  margin-inline: auto;
+  width: 90%;
+`;
 
 function Layout() {
   return (
--- tests/google_test.js
+++
@@ -1,8 +0,0 @@
-Feature('Google');
-
-Scenario('Search “CodeceptJS”', ({ I }) => {
-  I.amOnPage('https://www.google.com/ncr');
-  I.fillField('[name="q"]', 'CodeceptJS');
-  I.click('Google Search');
-  I.see('codecept.io');
-});
