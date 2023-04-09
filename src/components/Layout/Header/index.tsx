24abe5f85c7dc68f05a9943fb57219b764e184f4
BORA <a52447879@gmail.com>
2023-04-09 14:38:50 +0900

    feat: Header component에 categories menu 추가

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,11 +1,22 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import useFetchCategories from '../../../hooks/useFetchCategories';
 
 const StyledNavigation = styled.nav`
-  display: flex;
+  width: 100%;
+  height: 70px;
+  
+  padding: 20px 0;
+  
+  border-bottom: 1px solid #bcbcbc;
+  
+  ul {
+    display: flex;
+    justify-content: space-around;
+  }
   
   a {
-    width: 20%;
+
     font-size: 2rem;
     padding: 1rem;
     
@@ -16,14 +27,39 @@ const StyledNavigation = styled.nav`
 `;
 
 function Header() {
+  const { categories } = useFetchCategories();
+
   return (
     <StyledNavigation>
-      <Link to="/">
-        home
-      </Link>
-      <Link to="/products">
-        products
-      </Link>
+      <ul>
+        <li>
+          <Link to="/">
+            home
+          </Link>
+        </li>
+        <li>
+          <Link to="/products">
+            products
+          </Link>
+          {!!categories.length && (
+            <ul>
+              {categories.map((category) => (
+                <li key={category.id}>
+                  <Link to={`/products?categoryId=${category.id}`}>
+                    {category.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          )}
+        </li>
+        <li>
+          <Link to="/cart">
+            cart
+          </Link>
+        </li>
+      </ul>
+
     </StyledNavigation>
   );
 }
