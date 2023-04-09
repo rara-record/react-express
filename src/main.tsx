9999b9ab3fa4e4773d8e410572332cd4e56a28d4
BORA <a52447879@gmail.com>
2023-04-09 14:13:37 +0900

    feat: main, setupTests.ts에 reflect-metadata추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom/client';
+import 'reflect-metadata';
 
 import App from './App';
 
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,4 +1,5 @@
 import server from './mocks/server';
+import 'reflect-metadata';
 
 beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
 
